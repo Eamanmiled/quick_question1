1ce0ad1d31642213d89c1b64840c5623669eda47
KindaBadAtThis <130555562+Eamanmiled@users.noreply.github.com>
2024-03-11 16:20:10 +0000

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,33 @@
+class BankAccount:
+    account_num = 0
+
+    def __init__(self, balance):
+        self.balance = balance
+        BankAccount.account_num = BankAccount.account_num+1
+        self.account_num = BankAccount.account_num
+
+    def __str__(self):
+        return "Account number: " + str(self.account_num) + "\nBalance: " + str(self.balance)
+
+    def withdraw(self, amount):
+        self. balance = self.balance - amount
+        return self.balance
+
+
+    def lodge(self, amount):
+        self. balance = self.balance + amount
+        return self.balance
+
+
+person1 = BankAccount(1000)
+
+person2 = BankAccount(1500)
+print(person1.__str__())
+print(person2.__str__())
+
+
+person2.withdraw(500)
+person1.lodge(500)
+
+print(person1.__str__())
+print(person2.__str__())
